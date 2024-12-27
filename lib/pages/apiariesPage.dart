9a0326a0a620d2b9fc9325f6c5b809b3202cd10e
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-27 16:54:46 +0200

    Flutter <> Create - apiariesPage for owner

---
+++ lib/pages/apiariesPage.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:hivemind_app/utils/helperWidgets.dart';
+import 'package:hivemind_app/widgets/FilledBtn.dart';
+import 'package:hivemind_app/widgets/ListItem.dart';
+import 'package:hivemind_app/widgets/navBar.dart';
+
+List<Map<String, String>> apiaries = [
+  {"label": "Apiary #1", "Beekeeper name": "Yehya"},
+  {"label": "Apiary #2", "Beekeeper name": "Hatem"},
+  {"label": "Apiary #3", "Beekeeper name": "Seri"},
+  {"label": "Apiary #4", "Beekeeper name": "Nour"},
+];
+
+class ApiariesPage extends StatefulWidget {
+  const ApiariesPage({super.key});
+
+  @override
+  State<ApiariesPage> createState() => _ApiariesPageState();
+}
+
+class _ApiariesPageState extends State<ApiariesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Apiaries"),
+      ),
+      body: Column(
+        children: [
+          addVerticalSpace(24),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                for (int i = 0; i < apiaries.length; i++)
+                  ListItem(
+                    data: apiaries[i],
+                    icon: "assest/icons/apiary_icon.png",
+                    onPress: () {},
+                  )
+              ],
+            ),
+          ),
+          FilledBtn(),
+          addVerticalSpace(24),
+          Navbar()
+        ],
+      ),
+    );
+  }
+}
