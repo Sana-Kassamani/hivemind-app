141e7a652b34796dd02537eb8a51b1968b4cd616
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-27 16:49:25 +0200

    Flutter <> Create - helperWidgets for addVerticalSpace, imageBox and iconBox

---
+++ lib/utils/HelperWidgets.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+Widget addVerticalSpace(double height) {
+  return SizedBox(
+    height: height,
+  );
+}
+
+Widget imageBox(String imagePath, Color color) {
+  return SizedBox(
+    width: 24,
+    height: 24,
+    child: Image.asset(
+      imagePath,
+      color: color,
+      fit: BoxFit.contain,
+    ),
+  );
+}
+
+Widget iconBox(IconData icon, Color color) {
+  return SizedBox(
+      width: 24,
+      height: 24,
+      child: Icon(
+        icon,
+        color: color,
+      ));
+}
