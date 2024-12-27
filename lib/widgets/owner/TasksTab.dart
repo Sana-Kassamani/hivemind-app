2f929a0466967fe701b94b93ce226ba3372f4d4e
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-27 19:22:52 +0200

    Flutter <> Edit - TasksTab to add custom fields acc to Tasks array

--- lib/widgets/owner/TasksTab.dart
+++ lib/widgets/owner/TasksTab.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:hivemind_app/utils/helperWidgets.dart';
 
@@ -11,26 +13,92 @@ class TasksTab extends StatefulWidget {
 class _TasksTabState extends State<TasksTab> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 30, right: 30),
-      child: Column(
-        children: [
-          ExpansionTile(
-            leading: iconBox(
-                Icons.check_circle, Theme.of(context).colorScheme.secondary),
-            title: Text(
-              "Task #1",
-              style: Theme.of(context).textTheme.labelMedium,
-            ),
-            subtitle: Text(
-              "Nov 14, 2023",
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-            children: [
-              Text("Heyyy"),
-            ],
-          )
-        ],
+    const List<Map<String, String>> tasks = [
+      {
+        "title": "Task #1",
+        "status": "Pending",
+        "content": "check this and that hive",
+        "comment": "",
+        "date": "Nov 13, 2024"
+      },
+      {
+        "title": "Task #2",
+        "status": "Done",
+        "content": "check this and that hive",
+        "comment": "No comment",
+        "date": "Nov 13, 2024"
+      },
+      {
+        "title": "Task #3",
+        "status": "Pending",
+        "content": "check this and that hive",
+        "comment": "",
+        "date": "Nov 13, 2024"
+      },
+      {
+        "title": "Task #4",
+        "status": "Done",
+        "content": "check this and that hive",
+        "comment": "hey this is a comment",
+        "date": "Nov 13, 2024"
+      },
+    ];
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.only(left: 30, right: 30),
+        child: ListView.separated(
+          itemCount: tasks.length,
+          itemBuilder: (context, index) {
+            return ExpansionTile(
+              expandedAlignment: Alignment.centerLeft,
+              leading: tasks[index]["status"] == "Pending"
+                  ? iconBox(Icons.circle_outlined,
+                      Theme.of(context).colorScheme.secondary)
+                  : iconBox(Icons.check_circle,
+                      Theme.of(context).colorScheme.secondary),
+              title: Text(
+                tasks[index]["title"]!,
+                style: Theme.of(context).textTheme.labelMedium,
+              ),
+              subtitle: Text(
+                tasks[index]["date"]!,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              children: [
+                tasks[index]["status"] == "Pending"
+                    ? Column(
+                        spacing: 20,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Status: ${tasks[index]["status"]!}",
+                              style: Theme.of(context).textTheme.labelMedium),
+                          Text(
+                            tasks[index]["content"]!,
+                            style: Theme.of(context).textTheme.labelMedium,
+                          ),
+                        ],
+                      )
+                    : Column(
+                        spacing: 20,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Status: ${tasks[index]["status"]!}",
+                              style: Theme.of(context).textTheme.labelMedium),
+                          Text(
+                            tasks[index]["content"]!,
+                            style: Theme.of(context).textTheme.labelMedium,
+                          ),
+                          Text(
+                            "Comment: ${tasks[index]["comment"]}",
+                            style: Theme.of(context).textTheme.labelMedium,
+                          ),
+                        ],
+                      )
+              ],
+            );
+          },
+          separatorBuilder: (context, index) => addVerticalSpace(10),
+        ),
       ),
     );
   }
