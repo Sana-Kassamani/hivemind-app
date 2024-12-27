0113d35d9a6e863e7f50d524ed9d9037d3f694c3
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-27 16:55:02 +0200

    Flutter <> Create - hivesList widget

---
+++ lib/widgets/HivesList.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:hivemind_app/widgets/ListItem.dart';
+
+class HivesList extends StatefulWidget {
+  const HivesList({super.key});
+
+  @override
+  State<HivesList> createState() => _HivesListState();
+}
+
+class _HivesListState extends State<HivesList> {
+  final List<Map<String, String>> hives = [
+    {"label": "Hive #1"},
+    {"label": "Hive #2"},
+    {"label": "Hive #3"},
+    {"label": "Hive #4"},
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+          itemCount: hives.length,
+          itemBuilder: (context, index) {
+            return ListItem(
+              data: hives[index],
+              icon: "assets/icons/hive_icon.png",
+              onPress: () {},
+            );
+          }),
+    );
+  }
+}
