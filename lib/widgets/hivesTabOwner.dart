fc32f4e222be33fd225c0f4bf8f164fa36e1b3ae
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-27 17:29:25 +0200

    Flutter <> Create - HivesTab for owner in apiaryPage

---
+++ lib/widgets/hivesTabOwner.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:hivemind_app/widgets/FilledBtn.dart';
+import 'package:hivemind_app/widgets/HivesList.dart';
+import 'package:hivemind_app/widgets/ListItem.dart';
+import 'package:hivemind_app/widgets/LocationCard.dart';
+
+class HivesTab extends StatefulWidget {
+  const HivesTab({super.key});
+
+  @override
+  State<HivesTab> createState() => _HivesTabState();
+}
+
+class _HivesTabState extends State<HivesTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        spacing: 24,
+        children: [
+          LocationCard(),
+          HivesList(),
+          FilledBtn(
+            text: "Add a New Hive",
+            icon: Icon(Icons.add),
+          )
+        ],
+      ),
+    );
+  }
+}
