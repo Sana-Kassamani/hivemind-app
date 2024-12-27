f8d84042bfa059799b2cf65932dc25701a7f6f86
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-27 16:53:55 +0200

    Flutter <> Create - LocationCard for apiary loc and weather

---
+++ lib/widgets/LocationCard.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:hivemind_app/utils/HelperWidgets.dart';
+import 'package:hivemind_app/utils/colors.dart';
+
+class LocationCard extends StatelessWidget {
+  const LocationCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: ColorManager.CARD_BG,
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.85,
+        padding: EdgeInsets.all(10),
+        child: Column(
+          spacing: 10,
+          children: [
+            Row(
+              spacing: 10,
+              children: [
+                iconBox(Icons.location_on_outlined,
+                    Theme.of(context).colorScheme.primary),
+                Text(
+                  "Barouk Cedars",
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelMedium
+                      ?.copyWith(fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+            Row(
+              spacing: 10,
+              children: [
+                iconBox(
+                    Icons.cloud_queue, Theme.of(context).colorScheme.primary),
+                Text(
+                  "Sunny",
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelMedium
+                      ?.copyWith(fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
