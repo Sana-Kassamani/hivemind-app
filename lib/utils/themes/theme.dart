7dc5ba36230f3037ea0a2fe851f669de4406472b
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-27 18:26:25 +0200

    Flutter <> Add - bodyMedium style in theme

--- lib/utils/themes/theme.dart
+++ lib/utils/themes/theme.dart
@@ -19,36 +19,41 @@ class ThemeManager {
       ),
       backgroundColor: ColorManager.APPBAR_BG,
     ),
-    iconTheme: IconThemeData(size: 24),
+    iconTheme: IconThemeData(size: 24, color: ColorManager.COLOR_SECONDARY),
     textTheme: TextTheme(
-      titleLarge: TextStyle(
-        fontSize: 24,
-        color: ColorManager.COLOR_SECONDARY,
-        fontFamily: 'Tomorrow',
-        fontWeight: FontWeight.w500,
-      ),
-      titleMedium: TextStyle(
-        color: ColorManager.COLOR_SECONDARY,
-      ),
-      labelLarge: TextStyle(
-        color: ColorManager.COLOR_SECONDARY,
-        fontSize: 16,
-        fontFamily: 'Comme',
-        fontWeight: FontWeight.w700,
-      ),
-      labelMedium: TextStyle(
-        color: ColorManager.COLOR_SECONDARY,
-        fontSize: 16,
-        fontFamily: 'Comme',
-        fontWeight: FontWeight.w600,
-      ),
-      labelSmall: TextStyle(
-        color: ColorManager.COLOR_SECONDARY,
-        fontSize: 11,
-        fontFamily: 'Comme',
-        fontWeight: FontWeight.w500,
-      ),
-    ),
+        titleLarge: TextStyle(
+          fontSize: 24,
+          color: ColorManager.COLOR_SECONDARY,
+          fontFamily: 'Tomorrow',
+          fontWeight: FontWeight.w500,
+        ),
+        titleMedium: TextStyle(
+          color: ColorManager.COLOR_SECONDARY,
+        ),
+        labelLarge: TextStyle(
+          color: ColorManager.COLOR_SECONDARY,
+          fontSize: 16,
+          fontFamily: 'Comme',
+          fontWeight: FontWeight.w700,
+        ),
+        labelMedium: TextStyle(
+          color: ColorManager.COLOR_SECONDARY,
+          fontSize: 16,
+          fontFamily: 'Comme',
+          fontWeight: FontWeight.w600,
+        ),
+        labelSmall: TextStyle(
+          color: ColorManager.COLOR_SECONDARY,
+          fontSize: 11,
+          fontFamily: 'Comme',
+          fontWeight: FontWeight.w500,
+        ),
+        bodyMedium: TextStyle(
+          color: ColorManager.SUBTITLE_COLOR,
+          fontSize: 12,
+          fontFamily: 'Comme',
+          fontWeight: FontWeight.w500,
+        )),
     filledButtonTheme: FilledButtonThemeData(
       style: ButtonStyle(
         backgroundColor: WidgetStatePropertyAll(ColorManager.COLOR_PRIMARY),
