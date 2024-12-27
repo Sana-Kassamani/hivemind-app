0f316725e5ea6108daaec7ce7f00e7d719a2e539
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-27 16:49:49 +0200

    Flutter <> Create - listItem widget for apiaries and hives list

---
+++ lib/widgets/ListItem.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:hivemind_app/utils/colors.dart';
+import 'package:hivemind_app/widgets/CircleIcon.dart';
+
+class ListItem extends StatelessWidget {
+  const ListItem(
+      {super.key,
+      required this.data,
+      required this.icon,
+      required this.onPress});
+  final data;
+  final icon;
+  final onPress;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      tileColor: Colors.white,
+      leading: ListIcon(
+        circleColor: ColorManager.ICON_BG,
+        iconColor: ColorManager.COLOR_PRIMARY,
+        imagePath: icon,
+      ),
+      title: Text(
+        data["label"],
+        style: Theme.of(context).textTheme.labelMedium,
+      ),
+      subtitle: data.containsKey("Beekeeper name")
+          ? Text(data["Beekeeper name"])
+          : null,
+      trailing: IconButton(
+        onPressed: onPress,
+        icon: Icon(Icons.chevron_right),
+        color: Theme.of(context).colorScheme.secondary,
+      ),
+    );
+  }
+}
