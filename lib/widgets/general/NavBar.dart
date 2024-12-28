f51229d36e4b070a7faf8a8d552b9e55984ce848
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-29 00:08:05 +0200

    Flutter <> Add - navBar for Beekeeper

--- lib/widgets/general/NavBar.dart
+++ lib/widgets/general/NavBar.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hivemind_app/utils/colors.dart';
 
-class Navbar extends StatefulWidget {
-  const Navbar({super.key});
+class NavbarOwner extends StatefulWidget {
+  const NavbarOwner({super.key});
 
   @override
-  State<Navbar> createState() => _NavbarState();
+  State<NavbarOwner> createState() => _NavbarOwnerState();
 }
 
-class _NavbarState extends State<Navbar> {
+class _NavbarOwnerState extends State<NavbarOwner> {
   var currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
@@ -23,6 +23,48 @@ class _NavbarState extends State<Navbar> {
         NavigationDestination(
           icon: Icon(Icons.bar_chart),
           label: "Dashboard",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.notifications_active_outlined),
+          label: "Alerts",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.settings_outlined),
+          label: "Settings",
+        ),
+      ],
+      selectedIndex: currentPageIndex,
+      onDestinationSelected: (int index) {
+        setState(() {
+          currentPageIndex = index;
+        });
+      },
+    );
+  }
+}
+
+class NavbarBeekeeper extends StatefulWidget {
+  const NavbarBeekeeper({super.key});
+
+  @override
+  State<NavbarBeekeeper> createState() => _NavbarBeekeeperState();
+}
+
+class _NavbarBeekeeperState extends State<NavbarBeekeeper> {
+  var currentPageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      backgroundColor: ColorManager.CARD_BG,
+      indicatorColor: ColorManager.ICON_BG,
+      destinations: [
+        NavigationDestination(
+          icon: Icon(Icons.hive),
+          label: "Hives",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.assignment_outlined),
+          label: "Tasks",
         ),
         NavigationDestination(
           icon: Icon(Icons.notifications_active_outlined),
