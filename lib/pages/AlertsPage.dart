768e0df9b7ec08258c2ef387fcf8c4b860c705d6
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-28 05:11:32 +0200

    Flutter <> Create - AlertsPage

---
+++ lib/pages/AlertsPage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:hivemind_app/utils/HelperWidgets.dart';
+import 'package:hivemind_app/widgets/general/AlertsList.dart';
+import 'package:hivemind_app/widgets/general/ClearTextBtn.dart';
+import 'package:hivemind_app/widgets/general/NavBar.dart';
+
+class AlertsPage extends StatefulWidget {
+  const AlertsPage({super.key});
+
+  @override
+  State<AlertsPage> createState() => _AlertsPageState();
+}
+
+class _AlertsPageState extends State<AlertsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Alerts"),
+      ),
+      body: Column(
+        children: [
+          addVerticalSpace(24),
+          ClearTextBtn(context, "Mark All as Read", () {}),
+          AlertsList(),
+          Navbar(),
+        ],
+      ),
+    );
+  }
+}
