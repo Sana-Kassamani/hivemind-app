bafa0384bede38148e5849029c22daa50073668b
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-28 04:34:46 +0200

    Flutter <> Edit - segmentedTab to have responsive width

--- lib/widgets/general/SegmentedTab.dart
+++ lib/widgets/general/SegmentedTab.dart
@@ -4,31 +4,38 @@ import 'package:hivemind_app/utils/colors.dart';
 
 class SegmentedTab extends StatelessWidget {
   const SegmentedTab(
-      {super.key, required this.selectedControl, required this.onValueChanged});
-
+      {super.key,
+      required this.selectedControl,
+      required this.onValueChanged,
+      required this.tabs});
+  final List<String> tabs;
   final int? selectedControl;
   final onValueChanged;
   @override
   Widget build(BuildContext context) {
     final style = Theme.of(context).textTheme.labelMedium;
+    final double width = (tabs.isNotEmpty
+        ? (MediaQuery.of(context).size.width * 0.8) / tabs.length
+        : 0);
     return Center(
       child: CupertinoSlidingSegmentedControl(
         groupValue: selectedControl,
         padding: EdgeInsets.all(10),
         children: {
-          0: buildSegment(style, 'Hives'),
-          1: buildSegment(style, 'Tasks'),
+          for (int i = 0; i < tabs.length; i++)
+            i: buildSegment(style, tabs[i], width),
         },
         backgroundColor: Colors.white,
         thumbColor: ColorManager.SCAFFOLD_BG,
         onValueChanged: onValueChanged,
+        proportionalWidth: true,
       ),
     );
   }
 }
 
-Widget buildSegment(TextStyle? style, String text) => Container(
-      padding: EdgeInsets.fromLTRB(35, 12, 35, 12),
+Widget buildSegment(TextStyle? style, String text, double width) => Container(
+      padding: EdgeInsets.fromLTRB(width * 0.2, 12, width * 0.2, 12),
       child: Text(
         text,
         style: style,
