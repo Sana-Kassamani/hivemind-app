b023a2a149ffff387f9df28b2e1d8afa46fce1c9
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-29 00:14:33 +0200

    Flutter <> Create - TasksPage for beekeeper to show custom apiary tasks

---
+++ lib/pages/beekeeper/TasksPage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:hivemind_app/utils/HelperWidgets.dart';
+import 'package:hivemind_app/widgets/general/TasksList.dart';
+
+class TasksPage extends StatefulWidget {
+  const TasksPage({super.key});
+
+  @override
+  State<TasksPage> createState() => _TasksPageState();
+}
+
+class _TasksPageState extends State<TasksPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Tasks"),
+      ),
+      body: Column(
+        children: [
+          addVerticalSpace(24),
+          TasksList(),
+        ],
+      ),
+    );
+    ;
+  }
+}
