d5e600a6a5b2ffbb5478e2f304b8cc258f0f8a18
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-29 00:52:51 +0200

    Flutter <> Edit - Navbar widgets to stateless widgets with index and onSelected arguements

--- lib/widgets/general/NavBar.dart
+++ lib/widgets/general/NavBar.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hivemind_app/utils/colors.dart';
 
-class NavbarOwner extends StatefulWidget {
-  const NavbarOwner({super.key});
+class NavbarOwner extends StatelessWidget {
+  const NavbarOwner({
+    super.key,
+    required this.selectedIndex,
+    required this.onItemSelected,
+  });
+  final selectedIndex;
+  final onItemSelected;
 
-  @override
-  State<NavbarOwner> createState() => _NavbarOwnerState();
-}
-
-class _NavbarOwnerState extends State<NavbarOwner> {
-  var currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return NavigationBar(
@@ -33,54 +33,45 @@ class _NavbarOwnerState extends State<NavbarOwner> {
           label: "Settings",
         ),
       ],
-      selectedIndex: currentPageIndex,
-      onDestinationSelected: (int index) {
-        setState(() {
-          currentPageIndex = index;
-        });
-      },
+      selectedIndex: selectedIndex,
+      onDestinationSelected: onItemSelected,
     );
   }
 }
 
-class NavbarBeekeeper extends StatefulWidget {
-  const NavbarBeekeeper({super.key});
+class NavbarBeekeeper extends StatelessWidget {
+  const NavbarBeekeeper({
+    super.key,
+    required this.selectedIndex,
+    required this.onItemSelected,
+  });
+  final selectedIndex;
+  final onItemSelected;
 
-  @override
-  State<NavbarBeekeeper> createState() => _NavbarBeekeeperState();
-}
-
-class _NavbarBeekeeperState extends State<NavbarBeekeeper> {
-  var currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return NavigationBar(
-      backgroundColor: ColorManager.CARD_BG,
-      indicatorColor: ColorManager.ICON_BG,
-      destinations: [
-        NavigationDestination(
-          icon: Icon(Icons.hive),
-          label: "Hives",
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.assignment_outlined),
-          label: "Tasks",
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.notifications_active_outlined),
-          label: "Alerts",
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.settings_outlined),
-          label: "Settings",
-        ),
-      ],
-      selectedIndex: currentPageIndex,
-      onDestinationSelected: (int index) {
-        setState(() {
-          currentPageIndex = index;
-        });
-      },
-    );
+        backgroundColor: ColorManager.CARD_BG,
+        indicatorColor: ColorManager.ICON_BG,
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.hive),
+            label: "Hives",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.assignment_outlined),
+            label: "Tasks",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.notifications_active_outlined),
+            label: "Alerts",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.settings_outlined),
+            label: "Settings",
+          ),
+        ],
+        selectedIndex: selectedIndex,
+        onDestinationSelected: onItemSelected);
   }
 }
