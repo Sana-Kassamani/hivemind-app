7ee63b8eed233ea94230e0396186bf1932db35ed
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-28 05:11:53 +0200

    Flutter <> Create - base for SettingsPage

---
+++ lib/pages/SettingsPage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:hivemind_app/utils/HelperWidgets.dart';
+import 'package:hivemind_app/widgets/general/NavBar.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: Column(
+        children: [
+          addVerticalSpace(24),
+          Navbar(),
+        ],
+      ),
+    );
+    ;
+  }
+}
