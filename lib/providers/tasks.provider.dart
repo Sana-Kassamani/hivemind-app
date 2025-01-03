1ef29a83dfc89cb096dabd453e9e676f473984c9
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 00:24:39 +0200

    Flutter <> Create - tasks provider to save tasks in local map

---
+++ lib/providers/tasks.provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:hivemind_app/models/task.model.dart';
+import 'package:hivemind_app/utils/parseDate.dart';
+
+class Tasks extends ChangeNotifier {
+  final Map<String, List<Task>> _tasks = {};
+
+  void save({apiaryId, tasks}) {
+    _tasks[apiaryId] = [];
+
+    for (int i = 0; i < tasks.length; i++) {
+      var task = tasks[i];
+
+      // parse date of task to Jan 01, 2000 format
+      String updatedDt = parseDate(date: task["date"]);
+
+      final newTask = Task(
+        id: task["_id"],
+        title: task["title"],
+        content: task["content"],
+        status: task["status"],
+        comment: task["comment"],
+        date: updatedDt,
+      );
+
+      _tasks[apiaryId]!.add(newTask);
+      print("New task ${newTask.toString()}");
+    }
+
+    notifyListeners();
+  }
+}
