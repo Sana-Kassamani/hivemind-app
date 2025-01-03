e218f78bdddf0da7b51ef8e8b67fea36eca8887c
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 00:19:30 +0200

    Flutter <> Edit - change apiary.provider to apiary.model

---
+++ lib/models/apiary.model.dart
@@ -0,0 +1,30 @@
+class Apiary {
+  final String id;
+  final String label;
+  final String location;
+  final String beekeeperName;
+
+  Apiary({
+    required this.id,
+    required this.label,
+    required this.location,
+    required this.beekeeperName,
+  });
+  String getId() => id;
+}
+
+// export class Apiary {
+//   _id: Types.ObjectId;
+
+//   @Prop({ type: String, unique: true, required: true })
+//   label: string;
+
+//   @Prop({ type: String, required: true })
+//   location: string;
+
+//   @Prop({ type: [HiveSchema], default: [] })
+//   hives: Hive[];
+
+//   @Prop({ type: [TaskSchema], default: [] })
+//   tasks: Task[];
+// }
