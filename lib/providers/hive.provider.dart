2ce5709fc4f0d38a8d9008b1299f27ce26e052a4
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-03 18:16:48 +0200

    Flutter <> Add - save function in hiveProvider

--- lib/providers/hive.provider.dart
+++ lib/providers/hive.provider.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hivemind_app/providers/iotDetails.provider.dart';
+import 'package:hivemind_app/utils/parseDate.dart';
+import 'package:provider/provider.dart';
 
 class Hive extends ChangeNotifier {
   final String id;
@@ -6,6 +9,7 @@ class Hive extends ChangeNotifier {
   final int numberOfFrames;
   final bool harvestStatus;
   final List<String> diseases;
+  final List<IotDetails> details;
   String lastHarvestDate;
 
   Hive({
@@ -15,7 +19,46 @@ class Hive extends ChangeNotifier {
     required this.harvestStatus,
     required this.lastHarvestDate,
     required this.diseases,
+    required this.details,
   });
+
+  List<Hive> save({context, hives}) {
+    List<Hive> hiveList = [];
+    for (int i = 0; i < hives.length; i++) {
+      var hive = hives[i];
+
+      String updatedDt;
+      // parse lastHarvestDate of Hive to Jan 01, 2000 format
+      if (hive.containsKey('lastHarvestDate')) {
+        updatedDt = parseDate(date: hive["lastHarvestDate"]);
+      } else {
+        updatedDt = "Not harvested yet";
+      }
+
+      List<String> diseases = [];
+      for (int j = 0; j < hive["diseases"].length; j++) {
+        diseases.add(hive["diseases"][j]);
+      }
+      List<IotDetails> iotDetailsList =
+          Provider.of<IotDetails>(context, listen: false)
+              .save(iotDetails: hive["iotDetails"]);
+
+      final newHive = Hive(
+        id: hive["_id"],
+        label: hive["label"],
+        numberOfFrames: hive["nbOfFrames"],
+        harvestStatus: hive["harvestStatus"],
+        lastHarvestDate: updatedDt,
+        diseases: diseases,
+        details: iotDetailsList,
+      );
+
+      hiveList.add(newHive);
+      print("New hive ${newHive.toString()}");
+    }
+    notifyListeners();
+    return hiveList;
+  }
 }
 // @Schema()
 // export class Hive {
