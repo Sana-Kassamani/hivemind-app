784e43a371e9b0a94fc9dd154a0b1f750ce564bc
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-03 02:28:11 +0200

    Flutter <> Create - TasksListOwner and TasksListBeekeeper for each user

--- lib/widgets/general/TasksList.dart
+++ lib/widgets/general/TasksList.dart
@@ -3,12 +3,11 @@ import 'package:hivemind_app/main.dart';
 import 'package:hivemind_app/utils/helperWidgets.dart';
 import 'package:hivemind_app/widgets/general/TaskExpansionTile.dart';
 
-class TasksList extends StatelessWidget {
-  const TasksList({super.key});
+class TasksListOwner extends StatelessWidget {
+  const TasksListOwner({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const isOwner = ISOWNER;
     const List<Map<String, String>> tasks = [
       {
         "title": "Task #1",
@@ -45,9 +44,57 @@ class TasksList extends StatelessWidget {
         child: ListView.separated(
           itemCount: tasks.length,
           itemBuilder: (context, index) {
-            return isOwner
-                ? TaskExpansionTileOwner(task: tasks[index])
-                : TaskExpansionTileBeekeeper(task: tasks[index]);
+            return TaskExpansionTileOwner(task: tasks[index]);
+          },
+          separatorBuilder: (context, index) => addVerticalSpace(10),
+        ),
+      ),
+    );
+  }
+}
+
+class TasksListBeekeeper extends StatelessWidget {
+  const TasksListBeekeeper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const List<Map<String, String>> tasks = [
+      {
+        "title": "Task #1",
+        "status": "Pending",
+        "content": "check this and that hive",
+        "comment": "",
+        "date": "Nov 13, 2024"
+      },
+      {
+        "title": "Task #2",
+        "status": "Done",
+        "content": "check this and that hive",
+        "comment": "No comment",
+        "date": "Nov 13, 2024"
+      },
+      {
+        "title": "Task #3",
+        "status": "Pending",
+        "content": "check this and that hive",
+        "comment": "",
+        "date": "Nov 13, 2024"
+      },
+      {
+        "title": "Task #4",
+        "status": "Done",
+        "content": "check this and that hive",
+        "comment": "hey this is a comment",
+        "date": "Nov 13, 2024"
+      },
+    ];
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.only(left: 30, right: 30),
+        child: ListView.separated(
+          itemCount: tasks.length,
+          itemBuilder: (context, index) {
+            return TaskExpansionTileBeekeeper(task: tasks[index]);
           },
           separatorBuilder: (context, index) => addVerticalSpace(10),
         ),
