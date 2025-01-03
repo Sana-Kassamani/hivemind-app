2161350f85cfc4ab1d1f0c41cb2f51441cb3218e
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-03 02:34:15 +0200

    Flutter <> Add - Beekeeper class

--- lib/providers/user.provider.dart
+++ lib/providers/user.provider.dart
@@ -4,29 +4,42 @@ import 'package:flutter/material.dart';
 import 'package:hivemind_app/utils/enums/UserTypes.dart';
 
 class User extends ChangeNotifier {
-  final String _id;
-  final UserTypes _userType;
-  final UserSettings _settings;
+  final String id;
+  final UserTypes userType;
+  final UserSettings settings;
 
-  User(
-    this._id,
-    this._userType,
-    this._settings,
-  );
-  String getId() => _id;
-  UserTypes getUserType() => _userType;
-  UserSettings getSettings() => _settings;
+  User({
+    required this.id,
+    required this.userType,
+    required this.settings,
+  });
+  String getId() => id;
+  UserTypes getUserType() => userType;
+  UserSettings getSettings() => settings;
 }
 
 class UserSettings {
-  bool _darkmode;
-  bool _alertsOn;
-  UserSettings(
-    this._darkmode,
-    this._alertsOn,
-  );
-  bool getIsDark() => _darkmode;
-  bool getIsOnAlerts() => _alertsOn;
-  void setDark(bool state) => _darkmode = state;
-  void setAlertsSetting(bool state) => _alertsOn = state;
+  bool darkmode;
+  bool alertsOn;
+  UserSettings({
+    required this.darkmode,
+    required this.alertsOn,
+  });
+  bool getIsDark() => darkmode;
+  bool getIsOnAlerts() => alertsOn;
+  void setDark(bool state) => darkmode = state;
+  void setAlertsSetting(bool state) => alertsOn = state;
+}
+
+class Beekeeper {
+  final String id;
+  final String username;
+  final String? assignedApiaryId;
+  Beekeeper(
+      {required this.id,
+      required this.username,
+      required this.assignedApiaryId});
+  String getId() => id;
+  String getUsername() => username;
+  String? getassignedApiaryId() => assignedApiaryId;
 }
