7814d40bbe4dc417cab1d7d2609799567e5e4127
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 00:21:17 +0200

    Flutter <> Edit - iotDetails provider to save to local list

--- lib/providers/iotDetails.provider.dart
+++ lib/providers/iotDetails.provider.dart
@@ -1,61 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:hivemind_app/models/iotDetail.model.dart';
 import 'package:hivemind_app/utils/parseDate.dart';
 
 class IotDetails extends ChangeNotifier {
-  final String id;
-  final String humidity;
-  final String temperature;
-  final String mass;
-  final String date;
+  final Map<String, List<IotDetail>> _iotDetails = {};
 
-  IotDetails({
-    required this.id,
-    required this.humidity,
-    required this.temperature,
-    required this.mass,
-    required this.date,
-  });
-
-  List<IotDetails> save({iotDetails}) {
-    List<IotDetails> iotDetailsList = [];
-
+  void save({hiveId, iotDetails}) {
+    _iotDetails[hiveId] = [];
     for (int i = iotDetails.length - 1; i >= 0; i--) {
       var detail = iotDetails[i];
 
       // parse date of iotDetail to Jan 01, 2000 format
-      DateTime = DateTime.parse(detail["date"]);
-      var newFormat = DateFormat("MMM dd, yyyy");
       String updatedDt = parseDate(date: detail["date"]);
-
-      final newDetail = IotDetails(
+      print("Detail $i is $detail");
+      print("here 7");
+      final newDetail = IotDetail(
         id: detail["_id"],
-        humidity: detail["humidity"],
-        temperature: detail["temperature"],
-        mass: detail["mass"],
+        humidity: detail["humidity"].toDouble(),
+        temperature: detail["temperature"].toDouble(),
+        mass: detail["mass"].toDouble(),
         date: updatedDt,
       );
+      print("here 8");
 
-      iotDetailsList.add(newDetail);
+      _iotDetails[hiveId]!.add(newDetail);
       print("New task ${newDetail.toString()}");
     }
 
     notifyListeners();
-    return iotDetailsList;
   }
 }
-
-// export class HiveDetails {
-//   _id: Types.ObjectId;
-
-//   @Prop({ type: Number, required: true })
-//   humidity: number;
-
-//   @Prop({ type: Number, required: true })
-//   temperature: number;
-
-//   @Prop({ type: Number, required: true })
-//   mass: number;
-
-//   @Prop({ type: Date, default: Date.now })
-//   date: Date;
-// }
