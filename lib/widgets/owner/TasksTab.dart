2b28bde3373d7b6b96a3f77794455ea4b4c9b5c1
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 23:31:57 +0200

    Flutter <> Edit - empty state if no tasks

--- lib/widgets/owner/TasksTab.dart
+++ lib/widgets/owner/TasksTab.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hivemind_app/providers/tasks.provider.dart';
 import 'package:hivemind_app/widgets/general/ClearTextBtn.dart';
 import 'package:hivemind_app/widgets/general/FilledBtn.dart';
 import 'package:hivemind_app/widgets/general/TasksList.dart';
+import 'package:hivemind_app/widgets/general/empty.state.dart';
+import 'package:provider/provider.dart';
 
 class TasksTab extends StatefulWidget {
   const TasksTab({super.key});
@@ -13,18 +16,32 @@ class TasksTab extends StatefulWidget {
 class _TasksTabState extends State<TasksTab> {
   @override
   Widget build(BuildContext context) {
+    String apiaryId = ModalRoute.of(context)!.settings.arguments as String;
     return Expanded(
-      child: Column(
-        children: [
-          ClearTextBtn(context, "Clear Completed", () {}),
-          TasksListOwner(),
-          FilledBtn(
-            text: "Add a New Task",
-            icon: Icon(Icons.add),
-            onPress: () {},
-          )
-        ],
-      ),
+      child: Consumer<Tasks>(
+          builder: (BuildContext context, Tasks value, Widget? child) {
+        return Column(
+          children: [
+            value.tasks[apiaryId]!.isEmpty
+                ? EmptyState()
+                : Expanded(
+                    child: Column(
+                      children: [
+                        ClearTextBtn(context, "Clear Completed", () {}),
+                        TasksListOwner(
+                          tasks: value.tasks[apiaryId],
+                        ),
+                      ],
+                    ),
+                  ),
+            FilledBtn(
+              text: "Add a New Task",
+              icon: Icon(Icons.add),
+              onPress: () {},
+            )
+          ],
+        );
+      }),
     );
   }
 }
