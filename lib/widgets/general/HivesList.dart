30e8be3ea44236e30312d43ef0806535cb9e2e54
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 23:26:29 +0200

    Flutter <> Edit - empty state if no hives

--- lib/widgets/general/HivesList.dart
+++ lib/widgets/general/HivesList.dart
@@ -8,6 +8,7 @@ import 'package:hivemind_app/providers/auth.provider.dart';
 import 'package:hivemind_app/providers/hives.provider.dart';
 import 'package:hivemind_app/utils/enums/UserTypes.dart';
 import 'package:hivemind_app/widgets/general/ListItem.dart';
+import 'package:hivemind_app/widgets/general/empty.state.dart';
 import 'package:provider/provider.dart';
 
 class HivesList extends StatefulWidget {
@@ -27,30 +28,32 @@ class _HivesListState extends State<HivesList> {
       child: Consumer<Hives>(
         builder: (BuildContext context, Hives value, Widget? child) {
           List<Hive>? hives = value.hives[apiaryId];
-          return ListView.builder(
-              itemCount: hives!.length,
-              itemBuilder: (context, index) {
-                return ListItem(
-                  data: hives[index],
-                  icon: "assets/icons/hive_icon.png",
-                  onPress: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => userType == UserTypes.Owner
-                            ? HivePageOwner()
-                            : HivePageBeekeeper(),
-                        settings: RouteSettings(
-                          arguments: {
-                            "hiveId": hives[index].getId,
-                            "apiaryId": apiaryId
-                          },
-                        ),
-                      ),
+          return hives!.isEmpty
+              ? EmptyState(context: context)
+              : ListView.builder(
+                  itemCount: hives.length,
+                  itemBuilder: (context, index) {
+                    return ListItem(
+                      data: hives[index],
+                      icon: "assets/icons/hive_icon.png",
+                      onPress: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => userType == UserTypes.Owner
+                                ? HivePageOwner()
+                                : HivePageBeekeeper(),
+                            settings: RouteSettings(
+                              arguments: {
+                                "hiveId": hives[index].getId,
+                                "apiaryId": apiaryId
+                              },
+                            ),
+                          ),
+                        );
+                      },
                     );
-                  },
-                );
-              });
+                  });
         },
       ),
     );
