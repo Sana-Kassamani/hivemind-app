405eafdd006cc1ce5ed9177994f55df766ca75f8
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 19:06:37 +0200

    Flutter <> Create - empty state widget

---
+++ lib/widgets/general/empty.state.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+Widget EmptyState({context}) {
+  return Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      spacing: 20,
+      children: [
+        SizedBox(
+          width: 100,
+          child: Image.asset(
+            "assets/images/empty_pic.png",
+            color: Colors.grey[400],
+            fit: BoxFit.contain,
+          ),
+        ),
+        Text(
+          "Oops! It's Empty",
+          style: Theme.of(context)
+              .textTheme
+              .titleSmall!
+              .copyWith(color: Colors.grey[400]),
+        )
+      ],
+    ),
+  );
+}
