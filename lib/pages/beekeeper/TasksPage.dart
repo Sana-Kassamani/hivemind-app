7b53749483404eeba2d9333c799bdb48b0a44dd7
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 23:23:35 +0200

    Flutter <> Edit - dynamic tasks list for beekeeper

--- lib/pages/beekeeper/TasksPage.dart
+++ lib/pages/beekeeper/TasksPage.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hivemind_app/providers/apiaries.provider.dart';
+import 'package:hivemind_app/providers/tasks.provider.dart';
 import 'package:hivemind_app/utils/HelperWidgets.dart';
 import 'package:hivemind_app/widgets/general/TasksList.dart';
+import 'package:hivemind_app/widgets/general/empty.state.dart';
+import 'package:provider/provider.dart';
 
 class TasksPage extends StatefulWidget {
   const TasksPage({super.key});
@@ -16,13 +20,23 @@ class _TasksPageState extends State<TasksPage> {
       appBar: AppBar(
         title: Text("My Tasks"),
       ),
-      body: Column(
-        children: [
-          addVerticalSpace(24),
-          TasksListBeekeeper(),
-        ],
-      ),
+      body: Consumer<Apiaries>(
+          builder: (BuildContext context, Apiaries apiaryValue, Widget? child) {
+        return Consumer<Tasks>(
+            builder: (BuildContext context, Tasks value, Widget? child) {
+          return apiaryValue.apiary == null ||
+                  value.tasks[apiaryValue.apiary!.getId()]!.isEmpty
+              ? EmptyState(context: context)
+              : Column(
+                  children: [
+                    addVerticalSpace(24),
+                    TasksListBeekeeper(
+                        tasks: value.filterPendingTasks(
+                            apiaryId: apiaryValue.apiary!.getId())),
+                  ],
+                );
+        });
+      }),
     );
-    ;
   }
 }
