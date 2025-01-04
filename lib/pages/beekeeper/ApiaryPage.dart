eb3f8364620164ca7b09223363cecab948584fb6
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 19:02:17 +0200

    Flutter <> Add - consume apiaries provider in Apiary page beekeeper

--- lib/pages/beekeeper/ApiaryPage.dart
+++ lib/pages/beekeeper/ApiaryPage.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hivemind_app/providers/apiaries.provider.dart';
 import 'package:hivemind_app/utils/helperWidgets.dart';
 import 'package:hivemind_app/widgets/general/CenterTitle.dart';
 import 'package:hivemind_app/widgets/general/HivesList.dart';
 import 'package:hivemind_app/widgets/general/LocationCard.dart';
 import 'package:hivemind_app/widgets/general/NavBar.dart';
 import 'package:hivemind_app/widgets/general/SegmentedTab.dart';
+import 'package:hivemind_app/widgets/general/empty.state.dart';
 import 'package:hivemind_app/widgets/owner/HivesTab.dart';
 import 'package:hivemind_app/widgets/owner/TasksTab.dart';
+import 'package:provider/provider.dart';
 
 class ApiaryPageBeekeeper extends StatefulWidget {
   const ApiaryPageBeekeeper({super.key});
@@ -22,17 +25,24 @@ class _ApiaryPageBeekeeperState extends State<ApiaryPageBeekeeper> {
       appBar: AppBar(
         title: Text("Hives"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          addVerticalSpace(24),
-          CenterTitle(titleText: "Apiary 1"),
-          addVerticalSpace(24),
-          LocationCard(),
-          addVerticalSpace(24),
-          HivesList()
-        ],
-      ),
+      body: Consumer<Apiaries>(
+          builder: (BuildContext context, Apiaries value, Widget? child) {
+        return value.apiary == null
+            ? EmptyState(context: context)
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  addVerticalSpace(24),
+                  CenterTitle(titleText: value.apiary!.getLabel()),
+                  addVerticalSpace(24),
+                  LocationCard(apiary: value.apiary!),
+                  addVerticalSpace(24),
+                  HivesList(
+                    apiaryId: value.apiary!.getId(),
+                  )
+                ],
+              );
+      }),
       // bottomNavigationBar: NavbarBeekeeper(),
     );
   }
