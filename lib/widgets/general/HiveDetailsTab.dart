cb5ef46300096fe6d680e3f0f74251ae0acfab89
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 23:26:03 +0200

    Flutter <> Edit - provide diseases to diseases card

--- lib/widgets/general/HiveDetailsTab.dart
+++ lib/widgets/general/HiveDetailsTab.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hivemind_app/providers/hives.provider.dart';
 import 'package:hivemind_app/utils/HelperWidgets.dart';
 import 'package:hivemind_app/widgets/general/DatesRow.dart';
 import 'package:hivemind_app/widgets/general/HiveDetailsCard.dart';
 import 'package:hivemind_app/widgets/general/HiveGrid.dart';
+import 'package:provider/provider.dart';
 
 class HiveDetailsTab extends StatefulWidget {
   const HiveDetailsTab({super.key});
@@ -14,6 +16,10 @@ class HiveDetailsTab extends StatefulWidget {
 class _HiveDetailsTabState extends State<HiveDetailsTab> {
   @override
   Widget build(BuildContext context) {
+    final Map<String, dynamic>? arg =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>?;
+    String apiaryId = arg?["apiaryId"];
+    String hiveId = arg?["hiveId"];
     return Expanded(
       child: ListView(padding: EdgeInsets.symmetric(horizontal: 20), children: [
         Column(
@@ -23,7 +29,14 @@ class _HiveDetailsTabState extends State<HiveDetailsTab> {
             addVerticalSpace(20),
             DatesRow(),
             addVerticalSpace(20),
-            DiseasesCard(context: context),
+            Consumer<Hives>(
+                builder: (BuildContext context, Hives value, Widget? child) {
+              return diseasesCard(
+                  context: context,
+                  diseasesList: value
+                      .getById(hiveId: hiveId, apiaryId: apiaryId)
+                      .diseases);
+            }),
           ],
         ),
       ]),
