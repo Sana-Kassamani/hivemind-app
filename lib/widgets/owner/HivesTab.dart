9174fddc65ad59c80bfae1c2c7be97bfa8ecc88f
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 23:31:19 +0200

    Flutter <> Edit - dynamic data in hivesTab

--- lib/widgets/owner/HivesTab.dart
+++ lib/widgets/owner/HivesTab.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hivemind_app/providers/apiaries.provider.dart';
 import 'package:hivemind_app/widgets/general/FilledBtn.dart';
 import 'package:hivemind_app/widgets/general/HivesList.dart';
 import 'package:hivemind_app/widgets/general/LocationCard.dart';
+import 'package:provider/provider.dart';
 
 class HivesTab extends StatefulWidget {
   const HivesTab({super.key});
@@ -13,19 +15,25 @@ class HivesTab extends StatefulWidget {
 class _HivesTabState extends State<HivesTab> {
   @override
   Widget build(BuildContext context) {
+    String apiaryId = ModalRoute.of(context)!.settings.arguments as String;
     return Expanded(
-      child: Column(
-        spacing: 24,
-        children: [
-          LocationCard(),
-          HivesList(),
-          FilledBtn(
-            text: "Add a New Hive",
-            icon: Icon(Icons.add),
-            onPress: () {},
-          )
-        ],
-      ),
+      child: Consumer<Apiaries>(
+          builder: (BuildContext context, Apiaries value, Widget? child) {
+        return Column(
+          spacing: 24,
+          children: [
+            LocationCard(apiary: value.getById(apiaryId: apiaryId)),
+            HivesList(
+              apiaryId: apiaryId,
+            ),
+            FilledBtn(
+              text: "Add a New Hive",
+              icon: Icon(Icons.add),
+              onPress: () {},
+            )
+          ],
+        );
+      }),
     );
   }
 }
