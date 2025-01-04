caa908f800c8edd805502c4a8b088828f657aea0
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 23:29:11 +0200

    Flutter <> Edit - remove static task lists

--- lib/widgets/general/TasksList.dart
+++ lib/widgets/general/TasksList.dart
@@ -1,50 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:hivemind_app/main.dart';
+import 'package:hivemind_app/providers/tasks.provider.dart';
 import 'package:hivemind_app/utils/helperWidgets.dart';
 import 'package:hivemind_app/widgets/general/TaskExpansionTile.dart';
+import 'package:hivemind_app/widgets/general/empty.state.dart';
+import 'package:provider/provider.dart';
 
 class TasksListOwner extends StatelessWidget {
-  const TasksListOwner({super.key});
-
+  const TasksListOwner({super.key, required this.tasks});
+  final tasks;
   @override
   Widget build(BuildContext context) {
-    const List<Map<String, String>> tasks = [
-      {
-        "title": "Task #1",
-        "status": "Pending",
-        "content": "check this and that hive",
-        "comment": "",
-        "date": "Nov 13, 2024"
-      },
-      {
-        "title": "Task #2",
-        "status": "Done",
-        "content": "check this and that hive",
-        "comment": "No comment",
-        "date": "Nov 13, 2024"
-      },
-      {
-        "title": "Task #3",
-        "status": "Pending",
-        "content": "check this and that hive",
-        "comment": "",
-        "date": "Nov 13, 2024"
-      },
-      {
-        "title": "Task #4",
-        "status": "Done",
-        "content": "check this and that hive",
-        "comment": "hey this is a comment",
-        "date": "Nov 13, 2024"
-      },
-    ];
     return Expanded(
       child: Container(
         margin: EdgeInsets.only(left: 30, right: 30),
         child: ListView.separated(
-          itemCount: tasks.length,
+          itemCount: tasks!.length,
           itemBuilder: (context, index) {
-            return TaskExpansionTileOwner(task: tasks[index]);
+            return TaskExpansionTileOwner(task: tasks![index]);
           },
           separatorBuilder: (context, index) => addVerticalSpace(10),
         ),
@@ -54,40 +27,10 @@ class TasksListOwner extends StatelessWidget {
 }
 
 class TasksListBeekeeper extends StatelessWidget {
-  const TasksListBeekeeper({super.key});
-
+  const TasksListBeekeeper({super.key, this.tasks});
+  final tasks;
   @override
   Widget build(BuildContext context) {
-    const List<Map<String, String>> tasks = [
-      {
-        "title": "Task #1",
-        "status": "Pending",
-        "content": "check this and that hive",
-        "comment": "",
-        "date": "Nov 13, 2024"
-      },
-      {
-        "title": "Task #2",
-        "status": "Done",
-        "content": "check this and that hive",
-        "comment": "No comment",
-        "date": "Nov 13, 2024"
-      },
-      {
-        "title": "Task #3",
-        "status": "Pending",
-        "content": "check this and that hive",
-        "comment": "",
-        "date": "Nov 13, 2024"
-      },
-      {
-        "title": "Task #4",
-        "status": "Done",
-        "content": "check this and that hive",
-        "comment": "hey this is a comment",
-        "date": "Nov 13, 2024"
-      },
-    ];
     return Expanded(
       child: Container(
         margin: EdgeInsets.only(left: 30, right: 30),
