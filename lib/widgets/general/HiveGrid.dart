df385f00d5e4040de6203a25641b1ad77440a2d9
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 19:07:04 +0200

    Flutter <> Edit - hivesGrid to consume providers

--- lib/widgets/general/HiveGrid.dart
+++ lib/widgets/general/HiveGrid.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hivemind_app/providers/hives.provider.dart';
+import 'package:hivemind_app/providers/iotDetails.provider.dart';
 import 'package:hivemind_app/utils/HelperWidgets.dart';
 import 'package:hivemind_app/utils/colors.dart';
+import 'package:hivemind_app/utils/detailsList.dart';
 import 'package:hivemind_app/widgets/general/CircleIcon.dart';
 import 'package:hivemind_app/widgets/general/HiveDetailsCard.dart';
+import 'package:provider/provider.dart';
 
 class HiveGrid extends StatefulWidget {
   const HiveGrid({super.key});
@@ -12,81 +16,44 @@ class HiveGrid extends StatefulWidget {
 }
 
 class _HiveGridState extends State<HiveGrid> {
-  List<Map<String, dynamic>> details = [
-    {
-      "title": "Temperature",
-      "content": "25.4°C",
-      "iconColor": ColorManager.TEMP_COLOR,
-      "circleColor": ColorManager.TEMP_BG,
-      "imagePath": "assets/icons/temperature_icon.png",
-    },
-    {
-      "title": "Humidity",
-      "content": "16%",
-      "iconColor": ColorManager.HUMIDITY_COLOR,
-      "circleColor": ColorManager.HUMIDITY_BG,
-      "imagePath": "assets/icons/humidity_icon.png",
-    },
-    {
-      "title": "Mass",
-      "content": "12.4 kg",
-      "iconColor": ColorManager.MASS_COLOR,
-      "circleColor": ColorManager.MASS_BG,
-      "imagePath": "assets/icons/mass_icon.png",
-    },
-    {
-      "title": "Weather",
-      "content": "Sunny",
-      "iconColor": ColorManager.WEATHER_COLOR,
-      "circleColor": ColorManager.WEATHER_BG,
-      "imagePath": "assets/icons/weather_icon.png",
-    },
-    {
-      "title": "Harvest Status",
-      "content": "Pending",
-      "iconColor": ColorManager.HONEY_COLOR,
-      "circleColor": ColorManager.HONEY_BG,
-      "imagePath": "assets/icons/honey_icon.png",
-    },
-    {
-      "title": "Number of Frames",
-      "content": "12",
-      "iconColor": ColorManager.FRAME_COLOR,
-      "circleColor": ColorManager.FRAME_BG,
-      "imagePath": "assets/icons/frame_icon.png",
-    },
-    // {
-    //   "title": "Varroa mites",
-    //   "content": "Diseases",
-    //   "iconColor": ColorManager.DISEASES_COLOR,
-    //   "circleColor": ColorManager.DISEASES_BG,
-    //   "imagePath": "assets/icons/diseases_icon.png",
-    // },
-  ];
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: GridView.builder(
-        itemCount: details.length,
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: MediaQuery.of(context).size.width * 0.7,
-          // mainAxisExtent: 100,
-          childAspectRatio: 1.5,
-          crossAxisSpacing: 12,
-          mainAxisSpacing: 20,
-        ),
-        itemBuilder: (context, index) {
-          return HiveDetailsCard(
-              context: context,
-              iconColor: details[index]["iconColor"],
-              circleColor: details[index]["circleColor"],
-              imagePath: details[index]["imagePath"],
-              title: details[index]["title"],
-              content: details[index]["content"]);
-        },
-      ),
-    );
+    final Map<String, dynamic>? arg =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>?;
+    String apiaryId = arg?["apiaryId"];
+    String hiveId = arg?["hiveId"];
+    return Consumer<Hives>(
+        builder: (BuildContext context, Hives hiveValue, Widget? child) {
+      return SizedBox(
+        child: Consumer<IotDetails>(
+            builder: (BuildContext context, IotDetails value, Widget? child) {
+          final details = detailsList(
+            hive: hiveValue.getById(apiaryId: apiaryId, hiveId: hiveId),
+            detail: value.iotDetails[hiveId]![0],
+          );
+          return GridView.builder(
+            itemCount: details.length,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: MediaQuery.of(context).size.width * 0.7,
+              // mainAxisExtent: 100,
+              childAspectRatio: 1.5,
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 20,
+            ),
+            itemBuilder: (context, index) {
+              return HiveDetailsCard(
+                  context: context,
+                  iconColor: details[index]["iconColor"],
+                  circleColor: details[index]["circleColor"],
+                  imagePath: details[index]["imagePath"],
+                  title: details[index]["title"],
+                  content: details[index]["content"]);
+            },
+          );
+        }),
+      );
+    });
   }
 }
