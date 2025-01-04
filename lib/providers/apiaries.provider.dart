76d067dee7553b64de75f62cedf95f3b12d99cb7
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-04 14:33:24 +0200

    Flutter <> Add - save apiary for beekeeper assigned apiary

--- lib/providers/apiaries.provider.dart
+++ lib/providers/apiaries.provider.dart
@@ -10,22 +10,35 @@ import 'package:provider/provider.dart';
 
 class Apiaries extends ChangeNotifier {
   List<Apiary> _apiaries = [];
+  late Apiary apiary;
 
   List<Apiary> get apiariesList => _apiaries;
   void setApiariesList(apiaries) => _apiaries = apiaries;
 
-  Future load(context) async {
-    try {
-      final response =
-          await request(route: "/apiaries/owner", method: RequestMethods.get);
-      print(jsonDecode(response)["apiaries"]);
-      save(apiaries: jsonDecode(response)["apiaries"], context: context);
-    } catch (error) {
-      rethrow;
-    }
-  }
+  // Future loadApiary({context, apiaryId}) async {
+  //   try {
+  //     final response =
+  //         await request(route: "/apiaries/owner", method: RequestMethods.get);
 
-  void save({apiaries, context}) {
+  //     saveApiary(apiary: jsonDecode(response)["apiaries"], context: context);
+  //   } catch (error) {
+  //     rethrow;
+  //   }
+  // }
+
+  // Future loadApiaries({context}) async {
+  //   try {
+  //     final response =
+  //         await request(route: "/apiaries/owner", method: RequestMethods.get);
+
+  //     saveApiaries(
+  //         apiaries: jsonDecode(response)["apiaries"], context: context);
+  //   } catch (error) {
+  //     rethrow;
+  //   }
+  // }
+
+  void saveApiaries({context, apiaries}) {
     for (int i = 0; i < apiaries.length; i++) {
       var apiary = apiaries[i];
       Provider.of<Hives>(context, listen: false).save(
@@ -44,6 +57,22 @@ class Apiaries extends ChangeNotifier {
       _apiaries.add(newApiary);
     }
 
+    notifyListeners();
+  }
+
+  void saveApiary({context, apiary}) {
+    Provider.of<Hives>(context, listen: false).save(
+        context: context, apiaryId: apiary["_id"], hives: apiary["hives"]);
+    Provider.of<Tasks>(context, listen: false)
+        .save(apiaryId: apiary["_id"], tasks: apiary["tasks"]);
+
+    final newApiary = Apiary(
+      id: apiary["_id"],
+      label: apiary["label"],
+      location: apiary["location"],
+      beekeeperName: null,
+    );
+    apiary = newApiary;
     notifyListeners();
   }
 }
