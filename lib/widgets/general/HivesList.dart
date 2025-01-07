776842f0a48be9f733802f39a0beeb65348b7a6a
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-07 02:20:51 +0200

    Flutter <> Edit - SlidableListITem for hives in admin

--- lib/widgets/general/HivesList.dart
+++ lib/widgets/general/HivesList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hivemind_app/main.dart';
 import 'package:hivemind_app/models/hive.model.dart';
 import 'package:hivemind_app/pages/beekeeper/HivePage.dart';
@@ -33,26 +34,68 @@ class _HivesListState extends State<HivesList> {
               : ListView.builder(
                   itemCount: hives.length,
                   itemBuilder: (context, index) {
-                    return ListItem(
-                      data: hives[index],
-                      icon: "assets/icons/hive_icon.png",
-                      onPress: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => userType == UserTypes.Owner
-                                ? HivePageOwner()
-                                : HivePageBeekeeper(),
-                            settings: RouteSettings(
-                              arguments: {
-                                "hiveId": hives[index].getId,
-                                "apiaryId": apiaryId
+                    return userType == UserTypes.Owner
+                        ? Slidable(
+                            startActionPane: ActionPane(
+                              motion: StretchMotion(),
+                              children: [
+                                Theme(
+                                  data: Theme.of(context).copyWith(
+                                    outlinedButtonTheme:
+                                        const OutlinedButtonThemeData(
+                                      style: ButtonStyle(
+                                        iconColor: WidgetStatePropertyAll(
+                                            Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                  child: SlidableAction(
+                                      icon: Icons.delete,
+                                      backgroundColor: Colors.red,
+                                      onPressed: (context) {
+                                        // do something
+                                        print("slided");
+                                      }),
+                                ),
+                              ],
+                            ),
+                            child: ListItem(
+                              data: hives[index],
+                              icon: "assets/icons/hive_icon.png",
+                              onPress: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => HivePageOwner(),
+                                    settings: RouteSettings(
+                                      arguments: {
+                                        "hiveId": hives[index].getId,
+                                        "apiaryId": apiaryId
+                                      },
+                                    ),
+                                  ),
+                                );
                               },
                             ),
-                          ),
-                        );
-                      },
-                    );
+                          )
+                        : ListItem(
+                            data: hives[index],
+                            icon: "assets/icons/hive_icon.png",
+                            onPress: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => HivePageBeekeeper(),
+                                  settings: RouteSettings(
+                                    arguments: {
+                                      "hiveId": hives[index].getId,
+                                      "apiaryId": apiaryId
+                                    },
+                                  ),
+                                ),
+                              );
+                            },
+                          );
                   });
         },
       ),
