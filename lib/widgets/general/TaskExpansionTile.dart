cd56fe0be38ce2aca4b8a4f0df7d26e48313a0ee
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-07 15:14:19 +0200

    Flutter <> Add - state and setState for comment and completeTask in tasksPage

--- lib/widgets/general/TaskExpansionTile.dart
+++ lib/widgets/general/TaskExpansionTile.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hivemind_app/models/task.model.dart';
+import 'package:hivemind_app/providers/apiaries.provider.dart';
+import 'package:hivemind_app/providers/tasks.provider.dart';
 import 'package:hivemind_app/utils/HelperWidgets.dart';
+import 'package:hivemind_app/widgets/owner/alert.dialogue.dart';
+import 'package:provider/provider.dart';
 
 class TaskExpansionTileOwner extends StatelessWidget {
   const TaskExpansionTileOwner({super.key, required this.task});
@@ -52,8 +56,8 @@ class ExpandedTileOwner extends StatelessWidget {
             spacing: 20,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Status: ${task.status}",
-                  style: Theme.of(context).textTheme.labelMedium),
+              // Text("Status: ${task.status}",
+              //     style: Theme.of(context).textTheme.labelMedium),
               Text(
                 task.content,
                 style: Theme.of(context).textTheme.labelMedium,
@@ -67,16 +71,63 @@ class ExpandedTileOwner extends StatelessWidget {
   }
 }
 
-class TaskExpansionTileBeekeeper extends StatelessWidget {
+class TaskExpansionTileBeekeeper extends StatefulWidget {
   const TaskExpansionTileBeekeeper({super.key, required this.task});
 
   final Task task;
+
+  @override
+  State<TaskExpansionTileBeekeeper> createState() =>
+      _TaskExpansionTileBeekeeperState();
+}
+
+class _TaskExpansionTileBeekeeperState
+    extends State<TaskExpansionTileBeekeeper> {
+  String? comment;
+  void setComment(String _comment) {
+    setState(() {
+      comment = _comment;
+    });
+  }
+
+  Future completeTask(
+      {required taskId, required context, String? comment}) async {
+    try {
+      final apiaryId =
+          Provider.of<Apiaries>(context, listen: false).apiary!.getId();
+      await Provider.of<Tasks>(context, listen: false)
+          .completeTask(apiaryId: apiaryId, taskId: taskId, comment: comment);
+      print("ApiaryId $apiaryId");
+      print("task completed");
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Delete Hive failed: ${error.toString()}'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ExpansionTile(
       expandedAlignment: Alignment.centerLeft,
       leading: IconButton(
-        onPressed: () {},
+        onPressed: () async {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return CompleteTaskDialogue(
+                  onPressComplete: () => completeTask(
+                    taskId: widget.task.id,
+                    context: context,
+                    comment: comment,
+                  ),
+                  comment: comment,
+                );
+              });
+        },
         icon: Icon(
           Icons.circle_outlined,
           color: Theme.of(context).colorScheme.secondary,
@@ -87,31 +138,36 @@ class TaskExpansionTileBeekeeper extends StatelessWidget {
         ),
       ),
       title: Text(
-        task.title,
+        widget.task.title,
         style: Theme.of(context).textTheme.labelMedium,
       ),
       subtitle: Text(
-        task.date,
+        widget.task.date,
         style: Theme.of(context).textTheme.bodyMedium,
       ),
       children: [
-        ExpandedTileBeekeeper(task: task),
+        ExpandedTileBeekeeper(
+            task: widget.task, comment: comment, setComment: setComment),
       ],
     );
   }
 }
 
 class ExpandedTileBeekeeper extends StatefulWidget {
-  const ExpandedTileBeekeeper({super.key, required this.task});
-
+  ExpandedTileBeekeeper(
+      {super.key,
+      required this.task,
+      required this.comment,
+      required this.setComment});
+  String? comment;
   final Task task;
+  final setComment;
 
   @override
   State<ExpandedTileBeekeeper> createState() => _ExpandedTileBeekeeperState();
 }
 
 class _ExpandedTileBeekeeperState extends State<ExpandedTileBeekeeper> {
-  String comment = "";
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -129,9 +185,7 @@ class _ExpandedTileBeekeeperState extends State<ExpandedTileBeekeeper> {
                 Text("Comment", style: Theme.of(context).textTheme.labelSmall),
           ),
           onChanged: (value) {
-            setState(() {
-              comment = value;
-            });
+            widget.setComment(value);
           },
         ),
       ],
