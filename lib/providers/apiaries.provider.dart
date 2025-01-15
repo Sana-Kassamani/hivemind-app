c13dbb70eee35cf038ae14d80f32d96c70e60bb4
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-15 06:21:11 +0200

    Flutter <> Add - fetch weather upon saving newApiaries

--- lib/providers/apiaries.provider.dart
+++ lib/providers/apiaries.provider.dart
@@ -7,6 +7,7 @@ import 'package:hivemind_app/providers/tasks.provider.dart';
 import 'package:hivemind_app/utils/enums/RequestMethods.dart';
 import 'package:hivemind_app/utils/request.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 class Apiaries extends ChangeNotifier {
   List<Apiary> _apiaries = [];
@@ -38,55 +39,91 @@ class Apiaries extends ChangeNotifier {
   //     rethrow;
   //   }
   // }
+  Future<String?> getWeather({lat, lng}) async {
+    const String API_KEY = "e1374b46eeca4be471688468a060334f";
+    try {
+      String baseURL = 'https://api.openweathermap.org/data/2.5/weather';
+      String request =
+          '$baseURL?lat=$lat&lon=$lng&exclude=minutely,hourly,daily,alerts&appid=$API_KEY';
+      var response = await http.get(Uri.parse(request));
+      var data = jsonDecode(response.body);
+      print(response.body.toString());
+      if (response.statusCode == 200) {
+        print(response.body.toString());
+
+        String weather = data["weather"][0]["main"];
+        return weather;
+      } else {
+        throw Exception('Failed to load weather');
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
   Apiary getById({apiaryId}) {
     Apiary apiary = _apiaries.firstWhere((a) => a.id == apiaryId);
     return apiary;
   }
 
-  void saveApiaries({context, apiaries}) {
-    for (int i = 0; i < apiaries.length; i++) {
-      var apiary = apiaries[i];
-      Provider.of<Hives>(context, listen: false).save(
-          context: context, apiaryId: apiary["_id"], hives: apiary["hives"]);
-      Provider.of<Tasks>(context, listen: false)
-          .save(apiaryId: apiary["_id"], tasks: apiary["tasks"]);
-      String username = Provider.of<Beekeepers>(context, listen: false)
-          .findByAssignedApiary(id: apiary["_id"]);
+  Future saveApiaries({context, apiaries}) async {
+    try {
+      for (int i = 0; i < apiaries.length; i++) {
+        var apiary = apiaries[i];
+        Provider.of<Hives>(context, listen: false).save(
+            context: context, apiaryId: apiary["_id"], hives: apiary["hives"]);
+        Provider.of<Tasks>(context, listen: false)
+            .save(apiaryId: apiary["_id"], tasks: apiary["tasks"]);
+        String username = Provider.of<Beekeepers>(context, listen: false)
+            .findByAssignedApiary(id: apiary["_id"]);
 
-      final newApiary = Apiary(
-        id: apiary["_id"],
-        label: apiary["label"],
-        location: apiary["location"],
-        latitude: apiary["latitude"].toDouble(),
-        longitude: apiary["longitude"].toDouble(),
-        beekeeperName: username,
-      );
-      _apiaries.add(newApiary);
+        final newApiary = Apiary(
+          id: apiary["_id"],
+          label: apiary["label"],
+          location: apiary["location"],
+          latitude: apiary["latitude"].toDouble(),
+          longitude: apiary["longitude"].toDouble(),
+          beekeeperName: username,
+        );
+        final weather =
+            await getWeather(lat: newApiary.latitude, lng: newApiary.longitude);
+        newApiary.weather = weather;
+        _apiaries.add(newApiary);
+      }
+
+      notifyListeners();
+    } catch (error) {
+      rethrow;
     }
-
-    notifyListeners();
   }
 
-  void saveApiary({context, apiary}) {
-    if (apiary != null) {
-      Provider.of<Hives>(context, listen: false).save(
-          context: context, apiaryId: apiary["_id"], hives: apiary["hives"]);
-      Provider.of<Tasks>(context, listen: false)
-          .save(apiaryId: apiary["_id"], tasks: apiary["tasks"]);
+  Future saveApiary({context, apiary}) async {
+    try {
+      if (apiary != null) {
+        Provider.of<Hives>(context, listen: false).save(
+            context: context, apiaryId: apiary["_id"], hives: apiary["hives"]);
+        Provider.of<Tasks>(context, listen: false)
+            .save(apiaryId: apiary["_id"], tasks: apiary["tasks"]);
 
-      final newApiary = Apiary(
-        id: apiary["_id"],
-        label: apiary["label"],
-        location: apiary["location"],
-        latitude: apiary["latitude"].toDouble(),
-        longitude: apiary["longitude"].toDouble(),
-        beekeeperName: null,
-      );
-      _apiary = newApiary;
-    } else {
-      _apiary = null;
+        final newApiary = Apiary(
+          id: apiary["_id"],
+          label: apiary["label"],
+          location: apiary["location"],
+          latitude: apiary["latitude"].toDouble(),
+          longitude: apiary["longitude"].toDouble(),
+          beekeeperName: null,
+        );
+        final weather =
+            await getWeather(lat: newApiary.latitude, lng: newApiary.longitude);
+        newApiary.weather = weather;
+        _apiary = newApiary;
+      } else {
+        _apiary = null;
+      }
+      notifyListeners();
+    } catch (error) {
+      rethrow;
     }
-    notifyListeners();
   }
 
   Future addApiary({context, apiaryLabel, location, beekeeperId}) async {
@@ -122,6 +159,9 @@ class Apiaries extends ChangeNotifier {
           .save(context: context, apiaryId: newApiary.id, hives: []);
       Provider.of<Tasks>(context, listen: false)
           .save(apiaryId: newApiary.id, tasks: []);
+      final weather =
+          await getWeather(lat: newApiary.latitude, lng: newApiary.longitude);
+      newApiary.weather = weather;
       _apiaries.add(newApiary);
       notifyListeners();
     } catch (error) {
