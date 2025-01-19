c0635e7d61780892e326765d24a40151c1510ac2
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-19 03:40:00 +0200

    Flutter <> Edit - line chart consumer of iot details

--- lib/widgets/general/lineChart.dart
+++ lib/widgets/general/lineChart.dart
@@ -28,239 +28,194 @@ class _LineChartSample12State extends State<LineChartSample12> {
     super.initState();
   }
 
-  void _reloadData() async {
-    // final dataStr = await rootBundle.loadString(
-    //   'assets/data/btc_last_year_price.json',
-    // );
-
-    setState(() {
-      Map<String, List<IotDetail>> detailsMap =
-          Provider.of<IotDetails>(context).iotDetails;
-      _bitcoinPriceHistory = detailsMap[widget.hiveId]!.map((detail) {
-        return (detail.dateTime, detail.temperature);
-      }).toList();
-    });
-  }
+  // void _reloadData() {
+  //   setState(() {
+  //     Map<String, List<IotDetail>> detailsMap =
+  //         Provider.of<IotDetails>(context).iotDetails;
+  //     _bitcoinPriceHistory = detailsMap[widget.hiveId]!.map((detail) {
+  //       return (detail.dateTime, detail.temperature);
+  //     }).toList();
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    _reloadData();
     const leftReservedSize = 52.0;
     return Column(spacing: 16, children: [
       LayoutBuilder(builder: (context, constraints) {
-        final width = constraints.maxWidth;
-        //     return width >= 380
-        //         ? Row(
-        //             children: [
-        //               const SizedBox(width: leftReservedSize),
-        //               const _ChartTitle(),
-        //               const Spacer(),
-        //               Center(
-        //                 child: _TransformationButtons(
-        //                   controller: _transformationController,
-        //                 ),
-        //               ),
-        //             ],
-        //           )
-        //         : Column(
-        //             children: [
-        //               const _ChartTitle(),
-        //               const SizedBox(height: 16),
-        //               _TransformationButtons(
-        //                 controller: _transformationController,
-        //               ),
-        //             ],
-        //           );
-        //   },
-        // ),
-        // Padding(
-        //   padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        //   child: Row(
-        //     mainAxisAlignment: MainAxisAlignment.end,
-        //     spacing: 16,
-        //     children: [
-        //       const Text('Pan'),
-        //       Switch(
-        //         value: _isPanEnabled,
-        //         onChanged: (value) {
-        //           setState(() {
-        //             _isPanEnabled = value;
-        //           });
-        //         },
-        //       ),
-        //       const Text('Scale'),
-        //       Switch(
-        //         value: _isScaleEnabled,
-        //         onChanged: (value) {
-        //           setState(() {
-        //             _isScaleEnabled = value;
-        //           });
-        //         },
-        //       ),
-        //     ],
-        //   ),
-        // ),
-        return AspectRatio(
-          aspectRatio: 1,
-          child: Padding(
-            padding: const EdgeInsets.only(
-              top: 0.0,
-              right: 18.0,
-            ),
-            child: LineChart(
-              transformationConfig: FlTransformationConfig(
-                scaleAxis: FlScaleAxis.horizontal,
-                minScale: 1.0,
-                maxScale: 25.0,
-                panEnabled: _isPanEnabled,
-                scaleEnabled: _isScaleEnabled,
-                transformationController: _transformationController,
+        return Consumer<IotDetails>(builder:
+            (BuildContext context, IotDetails detailsValue, Widget? child) {
+          _bitcoinPriceHistory = detailsValue.iotDetails[widget.hiveId]!
+              .map((detail) {
+                return (detail.dateTime, detail.temperature);
+              })
+              .toList()
+              .reversed
+              .toList();
+
+          return AspectRatio(
+            aspectRatio: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 0.0,
+                right: 18.0,
               ),
-              LineChartData(
-                lineBarsData: [
-                  LineChartBarData(
-                    spots: _bitcoinPriceHistory?.asMap().entries.map((e) {
-                          final index = e.key;
-                          final item = e.value;
-                          final value = item.$2;
-                          return FlSpot(index.toDouble(), value);
-                        }).toList() ??
-                        [],
-                    dotData: const FlDotData(show: false),
-                    color: Colors.yellow,
-                    barWidth: 1,
-                    shadow: const Shadow(
-                      color: Colors.amberAccent,
-                      blurRadius: 2,
-                    ),
-                    belowBarData: BarAreaData(
-                      show: true,
-                      gradient: LinearGradient(
-                        colors: [
-                          Colors.yellow.shade100,
-                          Colors.yellow.shade400,
-                        ],
-                        stops: const [0.5, 1.0],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
+              child: LineChart(
+                transformationConfig: FlTransformationConfig(
+                  scaleAxis: FlScaleAxis.horizontal,
+                  minScale: 2.0,
+                  maxScale: 25.0,
+                  panEnabled: _isPanEnabled,
+                  scaleEnabled: _isScaleEnabled,
+                  transformationController: _transformationController,
+                ),
+                LineChartData(
+                  lineBarsData: [
+                    LineChartBarData(
+                      spots: _bitcoinPriceHistory?.asMap().entries.map((e) {
+                            final index = e.key;
+                            final item = e.value;
+                            final value = item.$2;
+                            return FlSpot(index.toDouble(), value);
+                          }).toList() ??
+                          [],
+                      dotData: const FlDotData(show: false),
+                      color: Colors.yellow,
+                      barWidth: 1,
+                      shadow: const Shadow(
+                        color: Colors.amberAccent,
+                        blurRadius: 2,
+                      ),
+                      belowBarData: BarAreaData(
+                        show: true,
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.yellow.shade100,
+                            Colors.yellow.shade400,
+                          ],
+                          stops: const [0.5, 1.0],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-                minY: 10,
-                maxY: 45,
-                lineTouchData: LineTouchData(
-                  touchSpotThreshold: 5,
-                  getTouchLineStart: (_, __) => -double.infinity,
-                  getTouchLineEnd: (_, __) => double.infinity,
-                  getTouchedSpotIndicator:
-                      (LineChartBarData barData, List<int> spotIndexes) {
-                    return spotIndexes.map((spotIndex) {
-                      return TouchedSpotIndicatorData(
-                        const FlLine(
-                          color: Colors.redAccent,
-                          strokeWidth: 1.5,
-                          dashArray: [8, 2],
-                        ),
-                        FlDotData(
-                          show: true,
-                          getDotPainter: (spot, percent, barData, index) {
-                            return FlDotCirclePainter(
-                              radius: 6,
-                              color: Colors.yellow,
-                              strokeWidth: 0,
-                              strokeColor: Colors.yellowAccent,
-                            );
-                          },
-                        ),
-                      );
-                    }).toList();
-                  },
-                  touchTooltipData: LineTouchTooltipData(
-                    getTooltipItems: (List<LineBarSpot> touchedBarSpots) {
-                      return touchedBarSpots.map((barSpot) {
-                        final date =
-                            _bitcoinPriceHistory![barSpot.x.toInt()].$1;
-                        return LineTooltipItem(
-                          '',
-                          const TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
+                  ],
+                  minY: 10,
+                  maxY: 45,
+                  lineTouchData: LineTouchData(
+                    touchSpotThreshold: 5,
+                    getTouchLineStart: (_, __) => -double.infinity,
+                    getTouchLineEnd: (_, __) => double.infinity,
+                    getTouchedSpotIndicator:
+                        (LineChartBarData barData, List<int> spotIndexes) {
+                      return spotIndexes.map((spotIndex) {
+                        return TouchedSpotIndicatorData(
+                          const FlLine(
+                            color: Colors.redAccent,
+                            strokeWidth: 1.5,
+                            dashArray: [8, 2],
                           ),
-                          children: [
-                            TextSpan(
-                              text:
-                                  '${date.year}/${date.month}/${date.day} \n ${date.hour}:${date.minute}',
-                              style: TextStyle(
-                                color: Colors.amber,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 12,
-                              ),
-                            ),
-                            TextSpan(
-                              text: '\n${barSpot.y} °C',
-                              style: const TextStyle(
-                                color: Color.fromARGB(255, 209, 190, 24),
-                                fontWeight: FontWeight.bold,
-                                fontSize: 12,
-                              ),
-                            ),
-                          ],
+                          FlDotData(
+                            show: true,
+                            getDotPainter: (spot, percent, barData, index) {
+                              return FlDotCirclePainter(
+                                radius: 6,
+                                color: Colors.yellow,
+                                strokeWidth: 0,
+                                strokeColor: Colors.yellowAccent,
+                              );
+                            },
+                          ),
                         );
                       }).toList();
                     },
-                    getTooltipColor: (LineBarSpot barSpot) =>
-                        Colors.yellow.shade50,
-                  ),
-                ),
-                titlesData: FlTitlesData(
-                  show: true,
-                  rightTitles: const AxisTitles(
-                    sideTitles: SideTitles(showTitles: false),
-                  ),
-                  topTitles: const AxisTitles(
-                    sideTitles: SideTitles(showTitles: false),
-                  ),
-                  leftTitles: const AxisTitles(
-                    drawBelowEverything: true,
-                    sideTitles: SideTitles(
-                      showTitles: true,
-                      reservedSize: leftReservedSize,
-                      maxIncluded: false,
-                      minIncluded: false,
+                    touchTooltipData: LineTouchTooltipData(
+                      getTooltipItems: (List<LineBarSpot> touchedBarSpots) {
+                        return touchedBarSpots.map((barSpot) {
+                          final date =
+                              _bitcoinPriceHistory![barSpot.x.toInt()].$1;
+                          return LineTooltipItem(
+                            '',
+                            const TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            children: [
+                              TextSpan(
+                                text:
+                                    '${date.year}/${date.month}/${date.day} \n ${date.hour}:${date.minute}',
+                                style: TextStyle(
+                                  color: Colors.amber,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              TextSpan(
+                                text: '\n${barSpot.y} °C',
+                                style: const TextStyle(
+                                  color: Color.fromARGB(255, 209, 190, 24),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          );
+                        }).toList();
+                      },
+                      getTooltipColor: (LineBarSpot barSpot) =>
+                          Colors.yellow.shade50,
                     ),
                   ),
-                  bottomTitles: AxisTitles(
-                    sideTitles: SideTitles(
-                      showTitles: true,
-                      reservedSize: 50,
-                      maxIncluded: false,
-                      getTitlesWidget: (double value, TitleMeta meta) {
-                        final date = _bitcoinPriceHistory![value.toInt()].$1;
-                        return SideTitleWidget(
-                          space: 29,
-                          meta: meta,
-                          child: Transform.rotate(
-                            angle: -45 * 3.14 / 180,
-                            child: Text(
-                              '${date.month}/${date.day} \t ${date.hour}:${date.minute}',
-                              style: const TextStyle(
-                                color: Color.fromARGB(255, 129, 19, 12),
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
+                  titlesData: FlTitlesData(
+                    show: true,
+                    rightTitles: const AxisTitles(
+                      sideTitles: SideTitles(showTitles: false),
+                    ),
+                    topTitles: const AxisTitles(
+                      sideTitles: SideTitles(showTitles: false),
+                    ),
+                    leftTitles: const AxisTitles(
+                      drawBelowEverything: true,
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        reservedSize: leftReservedSize,
+                        maxIncluded: false,
+                        minIncluded: false,
+                      ),
+                    ),
+                    bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        reservedSize: 50,
+                        minIncluded: true,
+                        maxIncluded: false,
+                        getTitlesWidget: (double value, TitleMeta meta) {
+                          final date = _bitcoinPriceHistory![value.toInt()].$1;
+                          return SideTitleWidget(
+                            space: 29,
+                            meta: meta,
+                            child: Transform.rotate(
+                              angle: -45 * 3.14 / 180,
+                              child: Text(
+                                '${date.month}/${date.day} \t ${date.hour}:${date.minute}',
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.normal,
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                      ),
                     ),
                   ),
                 ),
+                duration: Duration.zero,
               ),
-              duration: Duration.zero,
             ),
-          ),
-        );
+          );
+        });
       }),
     ]);
   }
