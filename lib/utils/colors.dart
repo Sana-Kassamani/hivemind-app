3d5e68e50c2403bfcaa8aa8f9e7d58bf42ebd543
Sana <Lenovo@DESKTOP-I4OODFD>
2025-01-21 14:58:51 +0200

    Flutter <> Add - secrets.defaults file

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -37,7 +37,7 @@ class ColorManager {
   static final DISEASES_COLOR = ColorManager.convertColor(hexString: "000000");
   static final DISEASES_BG = ColorManager.convertColor(hexString: "B3B3B3");
   static final Dark_Scaffold = ColorManager.convertColor(hexString: "412F2B");
-  static final Dark_Card = ColorManager.convertColor(hexString: "713E35");
+  static final Dark_Card = ColorManager.convertColor(hexString: "644B47");
   static final Dark_AppBarText = ColorManager.convertColor(hexString: "D3D9DC");
   static final Dark_MainText = ColorManager.convertColor(hexString: "FFFFFF");
   static final Dark_SecText = ColorManager.convertColor(hexString: "D9D9D9");
